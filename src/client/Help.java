61430f49d777c843fade4539ed2d63f6a720480a
TonyZheng <tony684310@gmail.com>
2016-06-07 15:48:24 +0800

    help

--- src/client/Help.java
+++ src/client/Help.java
@@ -1,16 +1,20 @@
 package client;
 
+import processing.core.PImage;
+
 public class Help {
 	private float windowWidth = MyWindow.windowWidth, windowHeight = MyWindow.windowHeight;
 	private Gamestage parent;
+	private PImage inst;
 	
 	public Help(Gamestage gs){
 		parent = gs;
+		inst = parent.loadImage("res/help.png");
 	}
 	public void display(){
 		parent.fill(0, 0, 128);
 		parent.textSize(50);
-		parent.text("About", windowWidth/2, windowHeight/4);
-		parent.text("How to play", windowWidth/2, windowHeight * 2/4);
+		parent.text("How to play", windowWidth/2, windowHeight * 1/4);
+		parent.image(inst, windowWidth/2 - 400, 200, 800, 600);
 	}
 }
